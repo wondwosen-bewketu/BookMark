fd3132b01a87d7addc2c082b2f0f71c324e3f2b3
Wondwosen Bewketu <wondwosen.bewketu@kegeberewtech.com>
2024-08-04 12:56:31 +0300

    Implement an authentication service to handle login and generate JWTs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
-    return this.authService.signup();
+  signup(@Body() dto: AuthDto) {
+    console.log({
+      dto,
+    });
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
@@ -15,3 +24,37 @@ export class AuthController {
     return this.authService.signin();
   }
 }
+
+// import {
+//   Controller,
+//   Post,
+//   Body,
+//   ParseIntPipe
+// } from '@nestjs/common';
+// import { AuthService } from './auth.service';
+// // import { AuthDto } from './dto';
+
+// @Controller('auth')
+// export class AuthController {
+//   constructor(private authService: AuthService) {}
+
+//   @Post('signup')
+//   signup(
+//     @Body('email') email: string,
+//     @Body('password', ParseIntPipe)
+//     password: string,
+//   ) {
+//     console.log({
+//       email,
+//       typeOfEmail: typeof email,
+//       password,
+//       typeOfPassword: typeof password,
+//     });
+//     return this.authService.signup();
+//   }
+
+//   @Post('signin')
+//   signin() {
+//     return this.authService.signin();
+//   }
+// }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,56 @@
-import { Injectable } from '@nestjs/common';
-import {User,Bookmark} from @PrismaClient
+import {
+  Prisma,
+  PrismaClient,
+} from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import {
+  Injectable,
+  ForbiddenException,
+} from '@nestjs/common';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { create } from 'domain';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
-  signin() {
-    return { msg: 'I Am Sign In' };
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    //generate the hash password
+    const hash = await argon.hash(dto.password);
+
+    try {
+      //save the new user on the DB
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        // select: {
+        //   id: true,
+        //   email: true,
+        //   createdAt: true,
+        // },
+      });
+
+      delete user.hash;
+      //return the saved user
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+    }
   }
 
-
-  signup() {
+  signin() {
     return { msg: 'I Am Sign Up' };
   }
-}import { Prisma, PrismaClient } from '@prisma/client';
-
+}
